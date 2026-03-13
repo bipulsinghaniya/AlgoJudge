13d68efdccf6eedf53cb41bb12a8db32579c5ebb
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-03-13 20:00:57 +0530

    i have added some new functionaliyt

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,15 @@
 import axios from "axios"
 
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:3000',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://lpuquora-backend.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
