c1171b57560459aef34335de16f9b944a5a0b8de
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-03-13 20:10:34 +0530

    i have added new functino

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -9,7 +9,7 @@ import axios from "axios"
 // });
 
 const axiosClient =  axios.create({
-    baseURL: 'https://lpuquora-backend.onrender.com',
+    baseURL: 'https://algojudge-backend.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
